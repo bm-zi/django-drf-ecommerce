01b13a596ca4b22f4a18c365a85ca50bce854589
bm-zi <bmzibmzi@hotmail.com>
2024-06-10 16:12:22 -0400

    nc

--- drfecommerce/drfecommerce/tests/conftest.py
+++ drfecommerce/drfecommerce/tests/conftest.py
@@ -3,24 +3,25 @@ from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
 from .factories import (
-    BrandFactory,
+    AttributeFactory,
+    AttributeValueFactory,
     CategoryFactory,
     ProductFactory,
     ProductImageFactory,
+    ProductLineAttributeValueFactory,
     ProductLineFactory,
-    AttributeFactory,
     ProductTypeFactory,
-    AttributeValueFactory,
 )
 
+
 register(CategoryFactory)
-register(BrandFactory)
 register(ProductFactory)
 register(ProductLineFactory)
 register(ProductImageFactory)
-register(AttributeFactory)
 register(ProductTypeFactory)
+register(AttributeFactory)
 register(AttributeValueFactory)
+register(ProductLineAttributeValueFactory)
 
 
 @pytest.fixture
