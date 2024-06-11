ddd9e948a7597a1469e41ae1b25898025b833c9e
bm-zi <bmzibmzi@hotmail.com>
2024-06-11 12:39:39 -0400

    nc

--- drfecommerce/drfecommerce/product/admin.py
+++ drfecommerce/drfecommerce/product/admin.py
@@ -1,66 +1,73 @@
-# from django.contrib import admin
-# from django.urls import reverse
-# from django.utils.safestring import mark_safe
+from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
-# from .models import (Category,
-#                      Product,
-#                      ProductLine,
-#                      ProductImage,
-#                      Attribute,
-#                      AttributeValue,
-#                      ProductType
-#                      )
-
-
-# class EditLinkInline():
-#     def edit(self, instance):
-#         url = reverse(
-#             f"admin:{instance._meta.app_label}_{instance._meta.model_name}_change", args=[instance.pk],
-#         )
-#         if instance.pk:
-#             link = mark_safe('<a href="{u}">edit</a>'.format(u=url))
-#             return link
-#         else:
-#             return ""
+from .models import (Category,
+                     Product,
+                     ProductLine,
+                     ProductImage,
+                     Attribute,
+                     AttributeValue,
+                     ProductType
+                     )
 
 
-# class ProductImageInline(admin.TabularInline):
-#     model = ProductImage
+class EditLinkInline():
+    def edit(self, instance):
+        url = reverse(
+            f"admin:{instance._meta.app_label}_{instance._meta.model_name}_change", args=[instance.pk],
+        )
+        if instance.pk:
+            link = mark_safe('<a href="{u}">edit</a>'.format(u=url))
+            return link
+        else:
+            return ""
 
 
-# class ProductLineInLine(EditLinkInline, admin.TabularInline):
-#     model = ProductLine
-#     readonly_fields = ("edit",)
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
 
 
-# class ProductAdmin(admin.ModelAdmin):
-#     inlines = [ProductLineInLine]
+class ProductLineInLine(EditLinkInline, admin.TabularInline):
+    model = ProductLine
+    readonly_fields = ("edit",)
 
 
-# class AttributeValueInline(admin.TabularInline):
-#     model = AttributeValue.product_line_attribute_value.through
+class AttributeValueInline(admin.TabularInline):
+    model = AttributeValue.product_line_attribute_value.through
 
 
-# class ProductLineAdmin(admin.ModelAdmin):
-#     inlines = [
-#         ProductImageInline,
-#         AttributeValueInline,
-#     ]
+class AttributeValueProductInline(admin.TabularInline):
+    model = AttributeValue.product_attr_value.through
 
 
-# class AttributeInline(admin.TabularInline):
-#     model = Attribute.product_type_attribute.through
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductLineInLine,
+        AttributeValueProductInline
+    ]
 
 
-# class ProductTypeAdmin(admin.ModelAdmin):
-#     inlines = [
-#         AttributeInline,
-#     ]
+class ProductLineAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductImageInline,
+        AttributeValueInline,
+    ]
 
 
-# admin.site.register(ProductLine, ProductLineAdmin)
-# admin.site.register(Product, ProductAdmin)
-# admin.site.register(Category)
-# admin.site.register(Attribute)
-# admin.site.register(ProductType, ProductTypeAdmin)
-# admin.site.register(AttributeValue)
+class AttributeInline(admin.TabularInline):
+    model = Attribute.product_type_attribute.through
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        AttributeInline,
+    ]
+
+
+admin.site.register(ProductLine, ProductLineAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Attribute)
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(AttributeValue)
