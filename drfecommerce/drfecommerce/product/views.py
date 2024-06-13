f87460dab3df700e536e868ba46a3ff6f72d1244
bm-zi <bmzibmzi@hotmail.com>
2024-06-13 15:15:00 -0400

    nc

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,24 +1,20 @@
-from django.db import connection
+# from django.db import connection
 from django.db.models import Prefetch
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from pygments import highlight
-from pygments.formatters import TerminalFormatter
-from pygments.lexers.sql import SqlLexer
-from sqlparse import format
+# from pygments import highlight
+# from pygments.formatters import TerminalFormatter
+# from pygments.lexers.sql import SqlLexer
+# from sqlparse import format
 
-from .models import Category, Product
-from .serializers import CategorySerializer, ProductSerializer
+from .models import Category, Product, ProductLine, ProductImage
+from .serializers import CategorySerializer, ProductSerializer, ProductCategorySerializer
 
 
 class CategoryViewSet(viewsets.ViewSet):
-    """
-    A simple Viewset for viewing all the categories
-    """
-
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().is_active()
 
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
@@ -28,45 +24,46 @@ class CategoryViewSet(viewsets.ViewSet):
 
 class ProductViewSet(viewsets.ViewSet):
     """
-    Viewset for viewing all products
+    A simple Viewset for viewing all products
     """
 
     queryset = Product.objects.all().is_active()
+
     lookup_field = "slug"
 
     def retrieve(self, request, slug=None):
         serializer = ProductSerializer(
             self.queryset.filter(slug=slug)
-            .select_related("category")
+            .prefetch_related(Prefetch("attribute_value__attribute"))
             .prefetch_related(Prefetch("product_line__product_image"))
             .prefetch_related(Prefetch("product_line__attribute_value__attribute")),
-            many=True,
+            many=True
         )
-
         data = Response(serializer.data)
-        ###
-        # q = list(connection.queries)
-        # print(len(q))
-        # for qs in q:
-        #     sqlformatted = format(str(qs["sql"]), reindent=True)
-        #     print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
-        ###
-        return data
 
-    @extend_schema(responses=ProductSerializer)
-    def list(self, request):
-        serializer = ProductSerializer(self.queryset, many=True)
-        return Response(serializer.data)
+        return data
 
     @action(
         methods=["get"],
         detail=False,
         url_path=r"category/(?P<slug>[\w-]+)",
     )
-    def list_product_by_category(self, request, slug=None):
+    def list_product_by_category_slug(self, request, slug=None):
         """
         An endpoint to return products by category
         """
-        serializer = ProductSerializer(
-            self.queryset.filter(category__slug=slug), many=True)
+        serializer = ProductCategorySerializer(
+            self.queryset.filter(category__slug=slug)
+            .prefetch_related(
+                Prefetch("product_line",
+                         queryset=ProductLine.objects.order_by("order"))
+            )
+            .prefetch_related(
+                Prefetch(
+                    "product_line__product_image",
+                    queryset=ProductImage.objects.filter(order=1),
+                )
+            ),
+            many=True,
+        )
         return Response(serializer.data)
--- drfecommerce/drfecommerce/tests/product/test_endpoints.py
+++ drfecommerce/drfecommerce/tests/product/test_endpoints.py
@@ -1,51 +1,28 @@
-# import pytest
-# import json
+import pytest
+import json
 
 
-# pytestmark = pytest.mark.django_db
-
-# class TestCategoryEndpoiunts:
-#     endpoint = "/api/category/"
-
-#     def test_category_get(self, category_factory, api_client):
-#         # Arrange
-#         category_factory.create_batch(4)
-#         # Act
-#         response= api_client().get(self.endpoint)
-#         # Assert
-#         assert response.status_code == 200
-#         print(json.loads(response.content))
-#         assert len(json.loads(response.content)) == 4
+pytestmark = pytest.mark.django_db
 
 
-# class TestProductEndpoiunts:
-#     endpoint = "/api/product/"
+class TestCategoryEndpoiunts:
+    endpoint = "/api/category/"
 
-#     # we changed the method name,
-#     # from test_product_get to test_return_all_products
-#     def test_return_all_products(self, product_factory, api_client):
-#         # Arrange
-#         product_factory.create_batch(4)
-#         # Act
-#         response= api_client().get(self.endpoint)
-#         # Assert
-#         assert response.status_code == 200
-#         print(json.loads(response.content))
-#         assert len(json.loads(response.content)) == 4
+    def test_category_get(self, category_factory, api_client):
+        category_factory.create_batch(4, is_active=True)
+        response = api_client().get(self.endpoint)
+        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 4
 
-#     # added new test for end-point /api/product/{slug}/
-#     def test_return_single_product_by_name(self, product_factory, api_client):
-#         obj = product_factory(slug="test-slug")
-#         response = api_client().get(f"{self.endpoint}{obj.slug}/")
-#         assert response.status_code == 200
-#         assert len(json.loads(response.content)) == 1
 
-#     # added new test for end-point /api/product/category/{category}/all/
-#     def test_return_products_by_category_slug(
-#         self, category_factory, product_factory, api_client
-#     ):
-#         obj = category_factory(slug="test-slug")
-#         product_factory(category=obj)
-#         response = api_client().get(f"{self.endpoint}category/{obj.slug}/")
-#         assert response.status_code == 200
-#         assert len(json.loads(response.content)) == 1
+class TestProductEndpoiunts:
+    endpoint = "/api/product/"
+
+    def test_return_products_by_category_slug(
+        self, category_factory, product_factory, api_client
+    ):
+        obj = category_factory(slug="test-slug")
+        product_factory(category=obj)
+        response = api_client().get(f"{self.endpoint}category/{obj.slug}/")
+        assert response.status_code == 200
+        assert len(json.loads(response.content)) == 1
